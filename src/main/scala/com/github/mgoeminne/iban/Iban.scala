930208272edfa9016d4aae4b04a6f168985a75ec
Mathieu Goeminne <mgoeminne@gmail.com>
2015-10-24 17:32:09 +0200

    Add a companion object that transforms a string into an IBAN account

--- src/main/scala/com/github/mgoeminne/iban/Iban.scala
+++ src/main/scala/com/github/mgoeminne/iban/Iban.scala
@@ -10,10 +10,56 @@ case class Iban(locale: Locale, check: Int, account: String)
    /**
     * @return a digital representation of the IBAN account
     */
-   def digital = locale.getCountry.toUpperCase + check + account
+   def digital = locale.getCountry.toUpperCase + "%02d".format(check) + account
 
    /**
     * @return a printable representation of the IBAN account
     */
-   override def toString = locale.getCountry + check + " " + account.grouped(4).map(_.toUpperCase).mkString(" ")
+   override def toString = locale.getCountry + "%02d".format(check) + " " + account.grouped(4).map(_.toUpperCase).mkString(" ")
+
+   def isValid = {
+      val head = digital.take(4)
+      val tail = digital.drop(4)
+
+      val t1 = tail + head
+      val t2 = replaceLetterByNumber(t1)
+      (BigInt(t2) mod 97) == 1
+   }
+
+
+   /**
+    * Replace all the letters [A-Z] by a number.
+    * A=10, B=11, etc.
+    * @param s a string
+    * @return the same string, in which all the letters have been replaced by numbers.
+    */
+   private def replaceLetterByNumber(s: String): String =
+   {
+      s.toUpperCase.map(character => {
+          if(character.isLetter) (character.toInt - 'A'.toInt + 10).toString
+          else character.toString
+      }).mkString
+   }
+
+   override def equals(o: Any) =
+   {
+       o match {
+          case i: Iban => this.locale.getCountry == i.locale.getCountry && this.check == i.check && this.account == i.account
+          case _ => false
+       }
+   }
+}
+
+object Iban
+{
+   def apply(s: String): Option[Iban] =
+   {
+      val cleaned = s.filter(c => c.isLetterOrDigit)
+
+      if((cleaned.size < 5) || (cleaned.size > 34)) return None
+      if(!cleaned(0).isLetter || !cleaned(1).isLetter) return None
+      if(!cleaned(2).isDigit || !cleaned(3).isDigit) return None
+
+      Some(new Iban(new Locale("", cleaned.take(2)), cleaned.drop(2).take(2).toInt, cleaned.drop(4)))
+   }
 }
--- src/test/scala/com/github/mgoeminne/iban/IbanTest.scala
+++ src/test/scala/com/github/mgoeminne/iban/IbanTest.scala
@@ -2,17 +2,50 @@ package com.github.mgoeminne.iban
 
 import java.util.Locale
 
-import org.scalatest.FlatSpec
+import org.scalatest.{Matchers, FlatSpec}
 
-class IbanTest extends FlatSpec {
+class IbanTest extends FlatSpec with Matchers {
 
    val lampiris = Iban(new Locale("fr", "BE"), 38, "001509424272")
+   val greekBank = Iban(new Locale("gr", "GR"), 3, "01108950000089529800453")
 
-   "An IBAN account" should "have a correct digital representation" in {
-       assert(lampiris.digital == "BE38001509424272")
+   "The Lampiris IBAN account" should "have a correct digital representation" in {
+       lampiris.digital should be ("BE38001509424272")
    }
 
    it should "have a correct printable representation" in {
-      assert(lampiris.toString == "BE38 0015 0942 4272")
+      lampiris.toString should be ("BE38 0015 0942 4272")
    }
+
+   it should "be a valid account" in {
+      lampiris should be a 'valid
+   }
+
+   it should "be created from a simple string" in {
+      Iban("BE38001509424272") should be a 'defined
+      Iban("BE38001509424272").get should equal (lampiris)
+      Iban("BE38 0015 0942 4272") should be a 'defined
+      Iban("BE38 0015 0942 4272").get should equal (lampiris)
+   }
+
+
+   "The IBAN account of the national greek bank" should "have a correct digital representation" in {
+      greekBank.digital should be ("GR0301108950000089529800453")
+   }
+
+   it should "have a correct printable representation" in {
+      greekBank.toString should be ("GR03 0110 8950 0000 8952 9800 453")
+   }
+
+   it should "be a valid account" in {
+      greekBank should be a 'valid
+   }
+
+   it should "be created from a simple string" in {
+      Iban("GR0301108950000089529800453") should be a 'defined
+      Iban("GR0301108950000089529800453").get should equal (greekBank)
+      Iban("GR03 0110 8950 0000 8952 9800 453") should be a 'defined
+      Iban("GR03 0110 8950 0000 8952 9800 453").get should equal (greekBank)
+   }
+
 }
