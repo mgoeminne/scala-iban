06b1c7916de02657f44d2524a9d7ff6e83d29b36
Mathieu Goeminne <mgoeminne@gmail.com>
2015-11-04 14:56:09 +0100

    Added an invalid Belgian BBAN for the tests

--- src/test/scala/com/github/mgoeminne/iban/BBANBelgiumTest.scala
+++ src/test/scala/com/github/mgoeminne/iban/BBANBelgiumTest.scala
@@ -10,6 +10,8 @@ class BBANBelgiumTest extends FlatSpec with Matchers with Inspectors
    val validBBANs = Seq("068249252641", "310126985517",
                         "539007547034")
 
+   val invalidBBANs = Seq("844-0103701-34")
+
    "All valid BBAN" should "pass the validation test" in {
       forAll(validBBANs){ x => new BBANBelgium(x).isValid shouldBe Some(true) }
    }
@@ -20,5 +22,9 @@ class BBANBelgiumTest extends FlatSpec with Matchers with Inspectors
              "310-1269855-17",
              "539-0075470-34")
       )
+   }
+
+   "All invalid BBAN" should "not pass the validation test" in {
+      forAll(invalidBBANs){ x=> new BBANBelgium(x).isValid shouldBe Some(false)}
    }
 }
